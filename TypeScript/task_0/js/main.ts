909c91e32899f2c4851811b29ce0fedd0eded572
euni-bit <seunielis@gmail.com>
2024-09-25 12:04:08 -0400

    task 0  twitching and changing use of <th>

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -10,35 +10,35 @@ const student1: Student = {
     lastName: "Martinez",
     age: 25,
     location: "Canada",
-} 
-
+};
 
 const student2: Student = {
     firstName: "Benito",
     lastName: "Martinez",
     age: 30,
     location: "Puerto Rico",
-}
+};
 
-const studentsList: Array<Student> = [student1,student2]
+const studentsList: Array<Student> = [student1, student2];
 
 const table: HTMLTableElement = document.createElement('table');
-
 document.body.appendChild(table);
 
 const thead: HTMLTableSectionElement = document.createElement('thead');
 thead.innerHTML = '<tr><th>firstName</th><th>location</th></tr>';
 table.appendChild(thead);
+
 const tbody: HTMLTableSectionElement = document.createElement('tbody');
 table.appendChild(tbody);
+
 for (let i: number = 0; i < studentsList.length; i++) {
     const row: HTMLTableRowElement = document.createElement('tr');
-    const th1: HTMLTableCellElement = document.createElement('th');
-    const th2: HTMLTableCellElement = document.createElement('th');
-    th1.innerHTML = studentsList[i].firstName;
-    th2.innerHTML = studentsList[i].location;
-    row.appendChild(th1);
-    row.appendChild(th2);
+    const td1: HTMLTableCellElement = document.createElement('td');
+    const td2: HTMLTableCellElement = document.createElement('td');
+    td1.innerHTML = studentsList[i].firstName;
+    td2.innerHTML = studentsList[i].location;
+    row.appendChild(td1);
+    row.appendChild(td2);
 
     tbody.appendChild(row);
 }
